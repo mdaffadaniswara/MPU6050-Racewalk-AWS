d3f9cbb8ebe7343b2ae0382c31c12e1b8ab058e8
Muhammad Daffa Daniswara <70484139+mdaffadaniswara@users.noreply.github.com>
2024-06-09 23:13:54 +0700

    update docker

--- docker/src/inference.py
+++ docker/src/inference.py
@@ -4,6 +4,6 @@ def predict(body: dict, model) -> dict:
     """
     Generate predictions for the incoming request using the model.
     """
-    data = pd.DataFrame(body)
+    data = pd.DataFrame([body])
     predictions = model.predict(data).tolist()
     return {"predictions": predictions}
